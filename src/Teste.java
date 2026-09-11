57a6a8192d4dfaccad0c4f38e91dc60acb88b2f4
Rodrigo122005 <rodrigorosiag@gmail.com>
2026-09-11 10:10:29 +0100

    Update Teste.java

--- src/Teste.java
+++ src/Teste.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Teste {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+        boolean isRunning;
+
+        while(isRunning){
+
+          System.out.print
+
+
+        }
+
+
+      
+
+
+  
